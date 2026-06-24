cb7e8213769c4b48c45fda60c942678b68cbe02a
MuhammadSaid-Almunawar <159503330+MuhammadSaid-Almunawar@users.noreply.github.com>
2026-06-24 15:51:35 +0700

    Create pointer-session.c

---
+++ sesi-05-function/pointer-session.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    int x = 10;
+    int *p = &x;
+
+    printf("p   = %p\n", p);
+    printf("p + 1  = %p\n", p + 1);
+    printf("p + 2  = %p\n", p + 2);
+    
+
+
+    return 0 ;
+
+}
